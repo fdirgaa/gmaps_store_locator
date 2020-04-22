73e89ce3a4e09345aa71b95d23cc94d2598c4472
Ferdi Dirgantara <ferdifordev@gmail.com>
2020-04-22 14:11:26 +0700

    initialize

---
+++ js/index.js
@@ -0,0 +1,51 @@
+var map;
+var markers = [];
+var infoWindow;
+var locationSelect;
+
+function initMap() {
+    var losAngeles = {
+        lat: 34.063380,
+        lng: -118.358080
+    };
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: losAngeles,
+        zoom: 11,
+        mapTypeId: 'roadmap',
+        mapTypeControlOptions: { style: google.maps.MapTypeControlStyle.DROPDOWN_MENU }
+    });
+
+    infoWindow = new google.maps.InfoWindow();
+
+    showStoreMarkers();
+}
+
+function showStoreMarkers(){
+    var bounds = new google.maps.LatLngBounds();
+    stores.forEach(function(store, index){
+        var latlng = new google.maps.LatLng(
+            store.coordinates.latitude,
+            store.coordinates.longitude);
+
+        var name = store.name;
+        var address = store.addressLines[0];
+        createMarker(latlng, name, address, index);
+        bounds.extend(latlng);
+    })
+    map.fitBounds(bounds);
+
+}
+
+function createMarker(latlng, name, address, index){
+    var html = "<b>" + name + "</b> <br/>" + address;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: latlng,
+      label: `${index+1}`
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+      infoWindow.setContent(html);
+      infoWindow.open(map, marker);
+    });
+    markers.push(marker);
+}
